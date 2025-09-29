1e1f1869dce66b0b35c56ef62b23e41b49bda7e4
GguilhermeS <guig75673@gmail.com>
2025-09-29 11:42:11 -0300

    EndPoints

--- Crud/Crud/src/main/java/AgendaMG/Crud/service/EventoService.java
+++ Crud/Crud/src/main/java/AgendaMG/Crud/service/EventoService.java
@@ -1,12 +1,12 @@
 package AgendaMG.Crud.service;
 
-import java.util.List;
-
+import AgendaMG.Crud.entity.Evento;
+import AgendaMG.Crud.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import AgendaMG.Crud.entity.Evento;
-import AgendaMG.Crud.repository.EventoRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventoService {
@@ -14,7 +14,37 @@ public class EventoService {
     @Autowired
     private EventoRepository eventoRepository;
 
-    public List<Evento> getAllEventosService() {
+    public Evento criarEvento(Evento evento) {
+        return eventoRepository.save(evento);
+    }
+
+    public List<Evento> listarEventos() {
         return eventoRepository.findAll();
+    }
+
+    public Optional<Evento> buscarPorId(int id) {
+        return eventoRepository.findById(id);
+    }
+
+    public Optional<Evento> atualizarEvento(int id, Evento eventoAtualizado) {
+        return eventoRepository.findById(id).map(evento -> {
+            evento.setNomeEvento(eventoAtualizado.getNomeEvento());
+            evento.setDescricaoDoEvento(eventoAtualizado.getDescricaoDoEvento());
+            evento.setDataDoEvento(eventoAtualizado.getDataDoEvento());
+            evento.setLocalDoEvento(eventoAtualizado.getLocalDoEvento());
+            evento.setPrecoDoEvento(eventoAtualizado.getPrecoDoEvento());
+            evento.setCapacidadeDePessoasNoEvento(eventoAtualizado.getCapacidadeDePessoasNoEvento());
+            evento.setTipoDoEvento(eventoAtualizado.getTipoDoEvento());
+            evento.setApresentadorDoEvento(eventoAtualizado.getApresentadorDoEvento());
+            evento.setDuracaoDoEvento(eventoAtualizado.getDuracaoDoEvento());
+            return eventoRepository.save(evento);
+        });
+    }
+
+    public boolean deletarEvento(int id) {
+        return eventoRepository.findById(id).map(evento -> {
+            eventoRepository.delete(evento);
+            return true;
+        }).orElse(false);
     }
 }
